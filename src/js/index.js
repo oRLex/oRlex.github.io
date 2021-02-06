6430d0d72b0ec5ef2dbb7b17c65a958eb780a5fb
oRlex <gumersinda88@gmail.com>
2021-02-06 21:12:44 +0200

    jquery updated

--- src/js/index.js
+++ src/js/index.js
@@ -3,12 +3,12 @@ $(document).ready(()=>{
   $('.slider__tab-wrap').on('click', '.slider__tab-item--link:not(.slider__tab-item--link-active)', function(event) {
     event.preventDefault();
     const selectedId = $(this).data('tab-id');
-    $('.slider__tab-item--link-active').removeClass('slider__tab-item--link-active')
+    $('.slider__tab-item--link-active').removeClass('slider__tab-item--link-active');
     $(this).addClass('slider__tab-item--link-active');
-    sliderContentEls.each(function(index){
-      $(this).removeClass('slider__content-active')
+    sliderContentEls.each(function(){
+      $(this).removeClass('slider__content-active');
       if ($(this).data('content-id') == selectedId) {
-        $(this).addClass('slider__content-active')
+        $(this).addClass('slider__content-active');
       }
     })
   });
@@ -20,13 +20,13 @@ $(document).ready(()=>{
     const icon = $(this).children('.icon');
 
     if (next.hasClass('tab-mobile__item-content--active')) {
-      next.removeClass('tab-mobile__item-content--active')
-      icon.removeClass('icon__up').addClass('icon__down')
+      next.removeClass('tab-mobile__item-content--active');
+      icon.removeClass('icon__up').addClass('icon__down');
     } else{
       $('.tab-mobile__item-content').removeClass('tab-mobile__item-content--active')
-      next.addClass('tab-mobile__item-content--active')
-      icon.addClass('icon__up').removeClass('icon__down')
-    }
+      next.addClass('tab-mobile__item-content--active');
+      icon.addClass('icon__up').removeClass('icon__down');
+    };
    
-  })
-})
+  });
+});
